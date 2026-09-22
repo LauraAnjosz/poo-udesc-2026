e991cf159ae66f28f237f0dd28c06e9fd35649c7
LauraAnjosz <laurabassanianjos@gmail.com>
2026-09-21 21:51:54 -0300

    Exercício 1 - Classe Retangulo e respectivo teste

---
+++ src/exercicio1oo/TestaRetangulo.java
@@ -0,0 +1,13 @@
+package exercicio1oo;
+
+public class TestaRetangulo {
+    public static void main(String[] args){
+        Retangulo medida = new Retangulo();
+
+        medida.largura = 12.6;
+        medida.altura = 9.8;
+
+        System.out.println("Largura: "+medida.largura);
+        System.out.println("Altura: "+medida.altura);
+    }
+}
