6fa6b4c1c6c9102ff681b3b103cd2ce8091776ac
LauraAnjosz <laurabassanianjos@gmail.com>
2026-09-21 21:55:10 -0300

    Exercicio 1 - Classe Circulo e respectivo teste

---
+++ src/exercicio1oo/TestaCirculo.java
@@ -0,0 +1,11 @@
+package exercicio1oo;
+
+public class TestaCirculo {
+    public static void main(String[] args){
+        Circulo medida = new Circulo();
+
+        medida.raio = 25.7;
+
+        System.out.println("Raio: "+medida.raio);
+    }
+}
